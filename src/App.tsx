3d2531d86c7a9c7bb527ad5cbdf81b52300fa44f
sogafish <k.sogafish@gmail.com>
2020-05-31 17:33:29 +0900

    :hammer: Routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,23 @@
 import React from 'react';
-import {
-  RecoilRoot,
-  // atom,
-  // selector,
-  // useRecoilState,
-  // useRecoilValue,
-} from 'recoil';
+// import {
+//   RecoilRoot,
+//   // atom,
+//   // selector,
+//   // useRecoilState,
+//   // useRecoilValue,
+// } from 'recoil';
+import { Route } from 'react-router';
+import { BrowserRouter } from 'react-router-dom'
+import { Home, Roster, Edit } from './pages';
 
 function App() {
   console.log(process);
   return (
-    <RecoilRoot>
-      <div>init</div>
-    </RecoilRoot>
+    <BrowserRouter>
+      <Route exact={true} path="/" component={Home} />
+      <Route path="/roster" component={Roster} />
+      <Route path="/edit" component={Edit} />
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/index.ts
@@ -0,0 +1,3 @@
+export { Home } from './Home';
+export { Roster } from './Roster';
+export { Edit } from './Edit';
